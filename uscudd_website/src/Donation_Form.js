3240c59b747b324e367e5bc9dbd4fcf5d3fe9918
gjmessino <gabyjmessino@gmail.com>
2026-05-13 16:18:38 -0700

    made Donation_Form

--- uscudd_website/src/Donation_Form.js
+++ uscudd_website/src/Donation_Form.js
@@ -1,13 +1,13 @@
-function Contact_Form() {
-    return(
+function Donation_Form() {
+    return (
         <div>
-            <h1>Contact Us</h1>
+            <h1>Donate</h1>
             <input type = "text">Name</input>
             <input type = "email">Email Address</input>
             <input type = "tel">Phone Number</input>
-            <input type = "text">Reason for Interest</input>
+            <input type = "number">Amount</input>
         </div>
     )
 }
 
-export default Contact_Form();
+export default Donation_Form();
